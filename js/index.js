1b486656b1dc262737887165db7f2cb95db34482
chou <a7700931@gmail.com>
2020-06-30 01:02:23 +0800

    game

--- js/index.js
+++ js/index.js
@@ -153,10 +153,7 @@ function createGround() {
 
   const groundGeometry = new THREE.PlaneGeometry(300, 300, 50, 50)
   const groundMaterial = new THREE.TextureLoader().load('img/grasslight-big.jpg')
-  groundMaterial.repeat.set(25, 25)
-  
   const skinMat = new THREE.MeshPhongMaterial( {map: groundMaterial} )
-
   let ground = new THREE.Mesh(groundGeometry, skinMat)
   ground.rotation.x = -Math.PI / 2
   ground.receiveShadow = true
